22285228de4a9122027cb71bdbd045e52eec8f37
[NamGwangJin] <[n45202605@gmail.com]>
2023-07-10 17:33:31 +0900

    0710

--- test01.py
+++ test01.py
@@ -1,2 +1,53 @@
-print('Hello New Python World!!!')
-print('남광진')
+
+# i, j, k, str1 = 0, 1, 3.14, 'hello world'
+# print("i ["+str(i)+"] j ["+str(j)+"] str1 ["+str1+"]")
+# print(str(i)+str1)
+# print(f"i [{i}] j [{j}] k [{k}] str1 [{str1}]")
+# print(f"i={i:2d} j={j:3d} k={k:4.2f} str1={str1:5s}")
+# print('i={0}, j={1}, k={2}, str1={3}'.format(i,j,k,str1))
+# print('i=%2d, j=%3d, k=%.2f, str1=%12s'%(i,j,k,str1))
+#
+# f = open('c:/temp/menu.txt','r')
+# while True:
+#     line = f.readline()
+#     if not line: break
+#     print(line,end='')
+# f.close()
+
+# for i in range(10): # == for(int i=0; i<10; 1++)
+#   print(i)
+
+# for i in range(2,10):
+#   print(f"---{i}단---")
+#   for j in range(1,10):
+#     print(f"{i} x {j} = {i*j:2d}")
+
+# i = 8//3 # 2
+# j = 8%3 # 2
+# k = 2**4 # 16
+# print(i,j,k)
+
+# i=2
+# j=2.0
+# k=3.14
+# m='240'
+# print(float(i),int(j),str(i),str(j),int(k))
+# print(int(m),float(m))
+
+# for i in range (1,100,2):
+#   print(i)
+#
+# for i in range(1,100):
+#   if i%2==0:
+#     continue
+#   print(i)
+
+# a=0; b=1
+# fibo=a+b
+# print(a)
+# print(b)
+# while fibo<1000 :
+#   print(fibo)
+#   a = b
+#   b = fibo
+#   fibo = a + b
