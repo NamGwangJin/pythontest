07bb86ddea6fca010f48945e684839fc15c3e516
NamGwangJin <n45202605@gmail.com>
2023-07-14 00:38:35 +0900

    recursive

---
+++ study.py
@@ -0,0 +1,28 @@
+# 재귀함수
+
+# 팩토리얼 구하기
+def fact(n):
+    if n == 1:
+        return 1
+    return n * fact(n - 1)
+
+# 피보나치 수 구하기
+def fib(n):
+    if n == 1 or n == 2:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+# n부터 1까지 출력하기
+def nOne(n):
+    if n == 1:
+        return print(1)
+    print(n)
+    nOne(n - 1)
+
+# 1부터 n까지 출력하기
+def oneN(n):
+    if n == 1:
+        return print(1)
+    oneN(n - 1)
+    print(n)
