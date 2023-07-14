92b9d47a43434a6a9cfaf7c926753002b206b785
[NamGwangJin] <[n45202605@gmail.com]>
2023-07-14 15:52:06 +0900

    0714 update

--- Menu.py
+++ Menu.py
@@ -1,16 +1,66 @@
-arMenu = []
-sum = 0
-while True: # 메뉴 추가
-    menu = input("메뉴명을 입력하세요.\n")
-    if menu == "":
-        print("메뉴 추가 종료. 메뉴판을 출력합니다.")
-        break
-    price = int(input("가격을 입력하세요.\n"))
-    arMenu.append( {'메뉴명': menu, '가격': price} )
+class Menu:
+    def __init__(self):
+        self.menuList = []
+        self.build()
+        # self.sum = 0
+    def build(self):
+        try:
+            f = open('c:/temp/menu.txt','r')
+            arMenu = f.readlines()
+            for i in range(len(arMenu)):
+                menu = arMenu[i].split(',')
+                self.menuList.append({'menu':menu[0],'price':int(menu[1])})
+            print('메뉴 불러오기 성공')
+        except:
+            print('메뉴를 불러오지 못했습니다.')
+        f.close()
 
-print('-'*10,'메뉴판','-'*10)
-for x in arMenu: # 메뉴판 출력
-    sum += x['가격']
-    print(f"{x['메뉴명']:15s} {x['가격']:5d}원")
+    def save(self):
+        f = open('c:/temp/menu.txt','w')
+        for i in self.menuList:
+            arMenu = i['menu']+","+str(i['price'])+'\n'
+            f.write(arMenu)
+        print('저장이 완료되었습니다.')
+        f.close()
 
-print(f"\n메뉴 금액 합계 : {sum}원")
+    def add(self):
+        while True:
+            name = input('추가할 메뉴명을 입력하세요. [enter: 메뉴 추가 종료]\n')
+            if name == '':
+                print('메뉴추가를 종료합니다.')
+                break
+            price = int(input(name+"의 가격을 입력하세요."))
+            self.menuList.append({'menu':name,'price':price})
+            print(name+" 메뉴 추가 완료")
+            self.display()
+
+    def display(self):
+        print('-' * 10, '메뉴판', '-' * 10)
+        for i in self.menuList:
+            # self.sum += i['price']
+            print(self.menuList.index(i)+1,"{0:>12}".format(i['menu']),f"{i['price']:5d}원")
+        # print(f"\n메뉴 총액 : {self.sum:8d}원")
+
+    def update(self):
+        self.display()
+        while True:
+            change = input('수정할 메뉴 번호를 입력해주세요. [enter: 수정 종료]')
+            if change == "":
+                print('메뉴 수정을 종료합니다.')
+                break
+            print(change+'번 메뉴 '+self.menuList[int(change)-1]['menu']+" 수정을 진행합니다.")
+            name = input('변경할 메뉴명을 입력해주세요.')
+            price = int(input(name+'의 가격을 입력해주세요.'))
+            self.menuList[int(change)-1] = {'menu':name,'price':price}
+            self.display()
+
+    def delete(self):
+        self.display()
+        while True:
+            delete = input('삭제할 메뉴 번호를 입력해주세요 [enter:삭제 종료]')
+            if delete == "":
+                print('메뉴 삭제를 종료합니다.')
+                break
+            del self.menuList[int(delete) - 1]
+            print(delete+'번 메뉴 삭제 완료.')
+            self.display()
--- test05.py
+++ test05.py
@@ -26,6 +26,21 @@ from datetime import date, datetime
 # else:
 #     print('잘못된 이메일 형식입니다.')
 
-now = datetime.now()
-print(now)
-print(now.strftime("%Y/%m/%d %H:%M:%S"))
+# now = datetime.now()
+# print(now)
+# print(now.strftime("%Y/%m/%d %H:%M:%S"))
+
+# pid=input('주민번호를 입력하시오')
+# n=2
+# sum=0
+# str1=''
+# for i in pid[:-1]:
+#     sum+=int(i)*n
+#     if str1!='':
+#         str1+='+'
+#     str1+=i+'x'+str(n)
+#     n+=1
+#     if n>9:
+#         n=2
+# print(11-sum%11,',',pid[-1])
+# print(str1)
