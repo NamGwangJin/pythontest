c5a51115057491326efc9ab58a86314cb594fc12
[NamGwangJin] <[n45202605@gmail.com]>
2023-07-14 17:25:51 +0900

    0714 update

--- Cafe.py
+++ Cafe.py
@@ -3,23 +3,62 @@ import Menu
 menu = Menu.Menu()
 
 while True:
-    ment = input('진행할 작업을 선택해주세요. [C:메뉴추가][R:메뉴판출력][U:메뉴수정][D:메뉴삭제][enter:종료]\n')
-    if ment == "C" or ment == "c":
-        menu.add()
+    work = input('진행할 작업을 선택해주세요. [M:메뉴관리][O:주문관리][S:매출관리][enter:종료]')
+    # 메뉴 관리
+    if work == 'M' or work == 'm':
+        while True:
+            ment = input('메뉴 관리 모드입니다. [C:메뉴추가][R:메뉴판출력][U:메뉴수정][D:메뉴삭제][enter:종료]\n')
+            if ment == "C" or ment == "c":
+                menu.add()
+                continue
+            elif ment == "R" or ment == 'r':
+                menu.display()
+                continue
+            elif ment == "U" or ment == 'u':
+                menu.update()
+                continue
+            elif ment == "D" or ment == 'd':
+                menu.delete()
+                continue
+            elif ment == "":
+                print('메뉴관리를 종료합니다.')
+                menu.save()
+                break
+            else:
+                print('잘못된 키를 입력하셨습니다.')
+                continue
+    # 주문 관리
+    elif work == 'O' or work == 'o':
+        while True:
+            ment = input('주문 관리 모드입니다. [C:주문하기][R:주문내역출력][U:주문내역수정][D:주문내역삭제][enter:종료]\n')
+            if ment == "C" or ment == "c":
+                # order.add()
+                continue
+            elif ment == "R" or ment == 'r':
+                # order.display()
+                continue
+            elif ment == "U" or ment == 'u':
+                # order.update()
+                continue
+            elif ment == "D" or ment == 'd':
+                # order.delete()
+                continue
+            elif ment == "":
+                print('주문관리를 종료합니다.')
+                menu.save()
+                break
+            else:
+                print('잘못된 키를 입력하셨습니다.')
+                continue
+    # 매출 관리
+    elif work == 'S' or work == 's':
+        # 매출내역 출력
         continue
-    elif ment == "R" or ment == 'r':
-        menu.display()
-        continue
-    elif ment == "U" or ment == 'u':
-        menu.update()
-        continue
-    elif ment == "D" or ment == 'd':
-        menu.delete()
-        continue
-    elif ment == "":
-        print('프로그램을 종료합니다.')
-        menu.save()
+    # 종료
+    elif work == "":
+        print('카페관리 프로그램을 종료합니다.')
         break
+    # 잘못된 키 입력
     else:
         print('잘못된 키를 입력하셨습니다.')
         continue
--- Menu.py
+++ Menu.py
@@ -10,17 +10,20 @@ class Menu:
             for i in range(len(arMenu)):
                 menu = arMenu[i].split(',')
                 self.menuList.append({'menu':menu[0],'price':int(menu[1])})
-            print('메뉴 불러오기 성공')
+            print('메뉴를 불러왔습니다.')
         except:
             print('메뉴를 불러오지 못했습니다.')
         f.close()
 
     def save(self):
-        f = open('c:/temp/menu.txt','w')
-        for i in self.menuList:
-            arMenu = i['menu']+","+str(i['price'])+'\n'
-            f.write(arMenu)
-        print('저장이 완료되었습니다.')
+        try:
+            f = open('c:/temp/menu.txt','w')
+            for i in self.menuList:
+                arMenu = i['menu']+","+str(i['price'])+'\n'
+                f.write(arMenu)
+            print('메뉴 저장이 완료되었습니다.')
+        except:
+            print('메뉴를 저장하지 못했습니다.')
         f.close()
 
     def add(self):
@@ -38,7 +41,7 @@ class Menu:
         print('-' * 10, '메뉴판', '-' * 10)
         for i in self.menuList:
             # self.sum += i['price']
-            print(self.menuList.index(i)+1,"{0:>12}".format(i['menu']),f"{i['price']:5d}원")
+            print(self.menuList.index(i) + 1,"{0:>12}".format(i['menu']),f"{i['price']:5d}원")
         # print(f"\n메뉴 총액 : {self.sum:8d}원")
 
     def update(self):
@@ -48,10 +51,10 @@ class Menu:
             if change == "":
                 print('메뉴 수정을 종료합니다.')
                 break
-            print(change+'번 메뉴 '+self.menuList[int(change)-1]['menu']+" 수정을 진행합니다.")
+            print(change+'번 메뉴 '+self.menuList[int(change) - 1]['menu']+" 수정을 진행합니다.")
             name = input('변경할 메뉴명을 입력해주세요.')
             price = int(input(name+'의 가격을 입력해주세요.'))
-            self.menuList[int(change)-1] = {'menu':name,'price':price}
+            self.menuList[int(change) - 1] = {'menu':name,'price':price}
             self.display()
         self.save()
 
